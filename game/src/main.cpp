3a549ae20784608852d3ace5166dfdcc0b1fa2be
ATaitambou <144388966+ATaitambou@users.noreply.github.com>
2025-09-11 11:36:26 -0400

    Update main.cpp
    Added acceleration velocity and the motion of the bird as a curve

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -28,11 +28,26 @@ int main()
 	launchposition.x = platform.x + platform.width - radius; // Launch at right end of platform doing the math for me
 	launchposition.y = platform.y - (platform.height - radius); // Launch at top of platform doing the math for me
 
+
+	Vector2 birdvelocity = { 100.0f, 0.0f }; // Initial velocity
+	Vector2 birdacceleration = { 0.0f, 9.81f }; // Gravity acceleration
+
 	while (!WindowShouldClose()) // Detect window close button or ESC key
     {
+
+
+		
 		float t = GetTime();  // Get time in seconds since InitWindow()
 		float dt = GetFrameTime(); // Get time in seconds for last frame drawn (delta time)
-		launchposition.x += 100 * dt; // Move right at 100 pixels per second
+		
+		//if (IsKeyPressed(KEY_SPACE))
+		//{
+		//	birdvelocity += birdacceleration * dt;// Change velocity when space is pressed
+		//	launchposition += birdvelocity * dt; // Change position based on velocity
+		//}
+
+		birdvelocity += birdacceleration * dt;// Change velocity when space is pressed
+		launchposition += birdvelocity * dt; // Change position based on velocity
 
         BeginDrawing();
             ClearBackground(WHITE); // Background changes
@@ -41,6 +56,7 @@ int main()
 			DrawRectangleRec(platform, BEIGE);
 			DrawRectangleRec(ground, BLUE);
 
+			
 
         EndDrawing();
     }
